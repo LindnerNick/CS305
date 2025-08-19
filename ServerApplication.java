4ca55030dcda6ecb7f34b244326cf9244e114e8b
LindnerNick <nicholas.g.lindner@gmail.com>
2025-08-18 22:23:58 -0500

    Add files via upload

---
+++ ServerApplication.java
@@ -0,0 +1,35 @@
+package com.snhu.sslserver;
+
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.tomcat.util.buf.HexUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
+
+@RestController
+class ServerController{
+//FIXME:  Add hash function to return the checksum value for the data string that should contain your name.    
+    @RequestMapping("/hash")
+    public String myHash() throws NoSuchAlgorithmException {
+    	String data = "Hello Nick Lindner!";
+    	MessageDigest msgDigest = MessageDigest.getInstance("SHA-256"); //Choice Algorithm
+        byte[] byteArr = msgDigest.digest(data.getBytes());
+        String checksum = HexUtils.toHexString(byteArr);
+        return "<p>data:"+data+"</p>"+
+        		"</p>checksum: "+checksum+"</p>";
+    }
+}
